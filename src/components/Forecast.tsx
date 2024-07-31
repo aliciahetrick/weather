fd50fa2c9ed293498d669285819e13e99ab8a693
Alicia Hetrick <aliciahetrick@gmail.com>
2024-07-30 23:40:53 -0400

    Styling - add cards

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -70,58 +70,68 @@ const Forecast = () => {
 
   return (
     <WrapperColumns>
-      <WrapperRows>
-        {hourlyPrecipitation &&
-          hourlyPrecipitation.map((listItem) => {
-            if (new Date(listItem.dt * 1000).getHours() > 12) {
-              return (
-                <WeeklyWeatherCard>
-                  {"  " +
-                    (new Date(listItem.dt * 1000).getHours() - 12) +
-                    " PM"}
-                  <div>{listItem.pop * 100}%</div>
-                </WeeklyWeatherCard>
-              );
-            }
-            return (
-              <WeeklyWeatherCard>
-                {"  " + new Date(listItem.dt * 1000).getHours() + " AM"}
-                <div>{listItem.pop * 100}%</div>
-              </WeeklyWeatherCard>
-            );
-          })}
-      </WrapperRows>
-      <WrapperRows>
-        {dailyWeather &&
-          dailyWeather.map((listItem) => {
-            return (
-              <WeeklyWeatherCard>
-                {dayNames[new Date(listItem.dt * 1000).getDay()]}
-                {"  " + new Date(listItem.dt * 1000).getDate()}
-                {/* <SVG src="/cloud.svg"></SVG> */}
-                {listItem.weather[0].main === "Clouds" && (
-                  <SVG src="/cloud.svg"></SVG>
-                )}
-                {listItem.weather[0].main === "Rain" && (
-                  <SVG src="/rain.svg"></SVG>
-                )}
-                {listItem.weather[0].main === "Snow" && (
-                  <SVG src="/snow.svg"></SVG>
-                )}
-                {listItem.weather[0].main === "Sun" && (
-                  <SVG src="/sun.svg"></SVG>
-                )}
-                {/* <SVG src="/rain.svg"></SVG>
+      <div>
+        <WrapperForecast>
+          <Title>Precipitation</Title>
+          <WrapperRows>
+            {hourlyPrecipitation &&
+              hourlyPrecipitation.map((listItem) => {
+                if (new Date(listItem.dt * 1000).getHours() > 12) {
+                  return (
+                    <WeeklyWeatherCard>
+                      {"  " +
+                        (new Date(listItem.dt * 1000).getHours() - 12) +
+                        " PM"}
+                      <div>{listItem.pop * 100}%</div>
+                    </WeeklyWeatherCard>
+                  );
+                }
+                return (
+                  <WeeklyWeatherCard>
+                    {"  " + new Date(listItem.dt * 1000).getHours() + " AM"}
+                    <div>{listItem.pop * 100}%</div>
+                  </WeeklyWeatherCard>
+                );
+              })}
+          </WrapperRows>
+        </WrapperForecast>
+      </div>
+      <div>
+        <WrapperForecast>
+          <Title>Forecast</Title>
+          <WrapperRows>
+            {dailyWeather &&
+              dailyWeather.map((listItem) => {
+                return (
+                  <WeeklyWeatherCard>
+                    {dayNames[new Date(listItem.dt * 1000).getDay()]}
+                    {"  " + new Date(listItem.dt * 1000).getDate()}
+                    {/* <SVG src="/cloud.svg"></SVG> */}
+                    {listItem.weather[0].main === "Clouds" && (
+                      <SVG src="/cloud.svg"></SVG>
+                    )}
+                    {listItem.weather[0].main === "Rain" && (
+                      <SVG src="/rain.svg"></SVG>
+                    )}
+                    {listItem.weather[0].main === "Snow" && (
+                      <SVG src="/snow.svg"></SVG>
+                    )}
+                    {listItem.weather[0].main === "Sun" && (
+                      <SVG src="/sun.svg"></SVG>
+                    )}
+                    {/* <SVG src="/rain.svg"></SVG>
                 <SVG src="/snow.svg"></SVG>
                 <SVG src="/sun.svg"></SVG>  */}
-                <HighAndLowTemp>
-                  <div>{Math.floor(listItem.main.temp_max)}</div>
-                  <div>{Math.floor(listItem.main.temp_min)}</div>
-                </HighAndLowTemp>
-              </WeeklyWeatherCard>
-            );
-          })}
-      </WrapperRows>
+                    <HighAndLowTemp>
+                      <div>{Math.floor(listItem.main.temp_max)}</div>
+                      <div>{Math.floor(listItem.main.temp_min)}</div>
+                    </HighAndLowTemp>
+                  </WeeklyWeatherCard>
+                );
+              })}
+          </WrapperRows>
+        </WrapperForecast>
+      </div>
     </WrapperColumns>
   );
 };
@@ -136,25 +146,44 @@ const WrapperColumns = styled.div`
 `;
 
 const WrapperRows = styled.div`
-  border: 5px solid #f1f1f8;
-  // box-shadow: 3px 3px #d28fff;
+  // border: 5px solid #f1f1f8;
+  // // box-shadow: 3px 3px #d28fff;
   border-radius: 0.5em;
   margin-left: 0.5em;
   margin-right: 0.5em;
   // background-color: #ffdbbb;
-  padding-top: 0.25em;
-  padding-bottom: 0.25em;
+  // padding-top: 0.25em;
+  // padding-bottom: 0.25em;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
   font-size: 60px;
   font-weight: 600;
-  margin-top: 30px;
-  margin-bottom: 30px;
+  // margin-top: 30px;
+  // margin-bottom: 30px;
   text-align: center;
 `;
 
+const WrapperForecast = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.div`
+  text-align: center;
+  font-size: 50px;
+  font-weight: 600;
+  font-family: "Raleway", sans-serif;
+  text-transform: uppercase;
+`;
+
 const WeeklyWeatherCard = styled.div`
+  background-color: #2e233d;
+  border: 5px solid #f1f1f8;
+  box-shadow: 3px 3px #d28fff;
+  border-radius: 0.5em;
+  padding-top: 0.25em;
+  padding-bottom: 0.25em;
   // border: 1px solid black;
   display: flex;
   flex-direction: column;
--- src/components/Today.tsx
+++ src/components/Today.tsx
@@ -41,20 +41,38 @@ const Today = () => {
     // return () => clearInterval(fetchDataInterval);
   }, []);
 
-  return (
-    <Wrapper>
-      <WrapperLeft>
-        {weather && <div>{Math.floor(weather.main.temp)}&deg;</div>}
-      </WrapperLeft>
-      <WrapperRight>
-        <DateTime weather={weather} />
+  // console.log("weatherfdj", weather.weather[0].main);
 
-        {weather && (
-          <WeatherEvent>{weather.weather[0].description}</WeatherEvent>
-        )}
-        <SunsetTime weather={weather} />
-      </WrapperRight>
-    </Wrapper>
+  return (
+    <>
+      <Title>Current</Title>
+      <Wrapper>
+        <WrapperLeft>
+          {weather && <div>{Math.floor(weather.main.temp)}&deg;</div>}
+        </WrapperLeft>
+        <WrapperMiddle>
+          {weather && (
+            <WeatherEvent>{weather.weather[0].description}</WeatherEvent>
+          )}
+          {weather && weather.weather[0].main === "Clouds" && (
+            <SVG src="/cloud.svg"></SVG>
+          )}
+          {weather && weather.weather[0].main === "Rain" && (
+            <SVG src="/rain.svg"></SVG>
+          )}
+          {weather && weather.weather[0].main === "Snow" && (
+            <SVG src="/snow.svg"></SVG>
+          )}
+          {weather && weather.weather[0].main === "Clear" && (
+            <SVG src="/sun.svg"></SVG>
+          )}
+        </WrapperMiddle>
+        <WrapperRight>
+          <DateTime weather={weather} />
+          <SunsetTime weather={weather} />
+        </WrapperRight>
+      </Wrapper>
+    </>
   );
 };
 
@@ -62,7 +80,17 @@ export default Today;
 
 const Wrapper = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
+  margin-left: 50px;
+  margin-right: 50px;
+`;
+
+const Title = styled.div`
+  text-align: center;
+  font-size: 50px;
+  font-weight: 600;
+  font-family: "Raleway", sans-serif;
+  text-transform: uppercase;
 `;
 
 const WeatherEvent = styled.div`
@@ -77,7 +105,32 @@ const WrapperLeft = styled.div`
   font-weight: 600;
   display: flex;
   align-items: center;
-  margin-right: 600px;
+  // margin-right: 600px;
+
+  background-color: #2e233d;
+  border: 5px solid #f1f1f8;
+  box-shadow: 3px 3px #d28fff;
+  border-radius: 0.25em;
+  // margin: 2em;
+  padding-left: 0.25em;
+  padding-right: 0.25em;
+`;
+
+const WrapperMiddle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 2em;
+  font-size: 80px;
+  // padding-top: 50px;
+  text-align: right;
+
+  background-color: #2e233d;
+  border: 5px solid #f1f1f8;
+  box-shadow: 3px 3px #d28fff;
+  border-radius: 0.25em;
+  // margin: 2em;
+  padding: 0.5em;
 `;
 
 const WrapperRight = styled.div`
@@ -85,6 +138,17 @@ const WrapperRight = styled.div`
   flex-direction: column;
   border-radius: 2em;
   font-size: 80px;
-  padding-top: 50px;
+  // padding-top: 50px;
   text-align: right;
+
+  background-color: #2e233d;
+  border: 5px solid #f1f1f8;
+  box-shadow: 3px 3px #d28fff;
+  border-radius: 0.25em;
+  // margin: 2em;
+  padding: 0.5em;
+`;
+
+const SVG = styled.img`
+  width: 100px;
 `;
